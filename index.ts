c7f31a8e414c4fc5aef09f9130228a1326c3a16f
SADAF SOHAIL <159582784+sadafrao@users.noreply.github.com>
2024-04-14 21:49:01 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,19 @@
+// Q75
+//this function used compound operate for 
+function usecompoundoperator(){
+    let x = 10;
+console.log("initial x:",x);
+x += 5;//add 5 to x
+console.log("x after adding 5:", x);
+x -= 1;
+console.log("x after subtracting 1:", x);
+x *= 2;
+console.log("x after multiplying by 2:", x);
+x /= 2;
+console.log("x after dividing by 2:", x);
+x %= 2;
+console.log("x after taking remainder:", x);
+x **= 2;
+console.log("x after taking power:", x);
+}
+usecompoundoperator();
